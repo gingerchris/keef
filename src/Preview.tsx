457994a569f517a40d1a51f5aca442c392120999
gingerchris <chrisdowling84@gmail.com>
2021-04-05 15:41:08 +0100

    Generate JS animations on iframe load

--- src/Preview.tsx
+++ src/Preview.tsx
@@ -1,4 +1,5 @@
-import React, { createRef, useEffect } from 'react';
+import React, { createRef } from 'react';
+import { useCurrentTime } from './context/currentTimeContext';
 
 interface PreviewProps {
   markup: string;
@@ -6,31 +7,45 @@ interface PreviewProps {
   animationStyles: string;
 }
 
-export const Preview = ({ markup, animationStyles }: PreviewProps) => {
+export const Preview = ({ markup, tracks }: PreviewProps) => {
   const iframeRef = createRef<HTMLIFrameElement>();
+  const { addAnimation } = useCurrentTime();
 
-  useEffect(() => {
-    if (iframeRef.current) {
-      // iframe initialised
-    }
-  }, [iframeRef]);
+  const onIframeLoad = () => {
+    const { contentWindow } = iframeRef.current!;
+    tracks.forEach((track) => {
+      const keyframes = track.keyframes
+        .reduce((keyframes: Keyframe[], keyframe: TrackKeyframe) => {
+          const frames = keyframe.percentages.map((percentage) => ({
+            ...keyframe.styles,
+            offset: percentage / 100,
+          }));
+          return [...keyframes, ...frames];
+        }, [])
+        .sort((a, b) => a.offset! - b.offset!);
+      const elements = track.selectors.flatMap((selector) => {
+        return Array.from(contentWindow!.document.querySelectorAll(selector));
+      });
+      elements.forEach((element) => addAnimation(element, keyframes));
+    });
+  };
+
   const srcDoc = `
     <!DOCTYPE html>
     <html lang="en" >
 
-    <head>
-
-    
-    <style>
-      ${animationStyles}
-    </style>
-
-    </head>
-
-    <body translate="no" >
+    <body id="keef_preview_body">
       ${markup}
     </body>
     </html>
   `;
-  return <iframe sandbox="allow-same-origin" srcDoc={srcDoc} ref={iframeRef} />;
+
+  return (
+    <iframe
+      onLoad={onIframeLoad}
+      sandbox="allow-same-origin"
+      srcDoc={srcDoc}
+      ref={iframeRef}
+    />
+  );
 };
