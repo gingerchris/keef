a732222039e600b9ea9aaf634e5d952d7f333adc
eddichen <contacteddichen@gmail.com>
2021-04-30 07:35:49 +0100

    Adding react-codemirror2 as the code editor, themes can be found at https://codemirror.net/theme/

--- src/MarkupInput.tsx
+++ src/MarkupInput.tsx
@@ -1,4 +1,9 @@
 import React from 'react';
+import { UnControlled as CodeMirror } from 'react-codemirror2';
+
+import 'codemirror/mode/xml/xml';
+import 'codemirror/lib/codemirror.css';
+import 'codemirror/theme/material.css';
 
 interface MarkupInputProps {
   markup: string;
@@ -7,8 +12,15 @@ interface MarkupInputProps {
 
 export const MarkupInput = ({ markup, setMarkup }: MarkupInputProps) => {
   return (
-    <pre style={{ overflow: 'auto' }}>
-      <code>{markup}</code>
-    </pre>
+    <CodeMirror
+      value={markup}
+      options={{
+        mode: 'xml',
+        theme: 'material',
+        lineNumbers: true,
+        lineWrapping: true,
+        tabSize: 2
+      }}
+    />
   );
 };
